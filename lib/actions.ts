9d6feeef6dfea1fc812003dc3be28317576a6afa
Dinann <notxurd46@gmail.com>
2025-01-26 23:59:28 +0700

    refactor(schema): make email unique

--- lib/actions.ts
+++ lib/actions.ts
@@ -3,25 +3,30 @@
 import { ActionResult } from "@/types/auth";
 import { loginSchema } from "@/types/validations";
 import { redirect } from "next/navigation";
+import { prisma } from "./prisma";
 
 export async function SignIn(
   _: unknown,
   formData: FormData,
 ): Promise<ActionResult> {
-  console.log(formData.get("email"));
-
-  // const validatedFields = loginSchema.safeParse({
-  //   email: formData.get("email"),
-  //   password: formData.get("password"),
-  // });
+  const validatedFields = loginSchema.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
 
   // Return early if the form data is invalid
-  // if (!validatedFields.success) {
-  //   const { errors } = validatedFields.error;
-  //   return {
-  //     error: errors[0].message,
-  //   };
-  // }
+  if (!validatedFields.success) {
+    const { errors } = validatedFields.error;
+    return {
+      error: errors[0].message,
+    };
+  }
+
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email: validatedFields.data.email,
+    },
+  });
 
   return redirect("/sign-in");
 }
--- types/validations.ts
+++ types/validations.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z.string().email({ message: "Invalid Email" }),
+  email: z
+    .string({ required_error: "Email is required" })
+    .email({ message: "Invalid Email" }),
   password: z
-    .string()
+    .string({ required_error: "Password is required" })
     .min(8, { message: "Password must be minimum of 8 characters" })
     .max(32, { message: "Password must be less than 32 characters" }),
 });
