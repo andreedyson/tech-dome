7dc91a7cfe9c8f82a840ca1fb1c3d19a4228970c
Dinann <notxurd46@gmail.com>
2025-02-13 01:05:08 +0700

    feat(landing): add testimonials to hero section

--- components/pages/Hero.tsx
+++ components/pages/Hero.tsx
@@ -2,52 +2,78 @@ import Image from "next/image";
 import React from "react";
 import { Crown, ShoppingCart } from "lucide-react";
 import { Button } from "../ui/button";
+import { LANDING_TESTIMONIALS } from "@/constants";
+import test from "node:test";
 
 function Hero() {
   return (
-    <section className="mt-8 grid w-full place-content-center place-items-center gap-6 max-lg:text-center lg:grid-cols-2 lg:justify-items-start">
-      {/* Header & CTA*/}
-      <div className="flex w-full flex-col max-lg:order-last max-lg:items-center max-lg:justify-center">
-        <div className="flex w-fit items-center gap-2 rounded-full border-2 bg-white px-3 py-2 text-sm font-semibold text-black shadow-md">
-          <Crown size={20} />
-          Most Popular Product in DealDome
+    <section className="mt-8 w-full">
+      <div className="grid place-content-center place-items-center gap-6 max-lg:text-center lg:grid-cols-2 lg:justify-items-start">
+        {/* Header & CTA*/}
+        <div className="flex w-full flex-col max-lg:order-last max-lg:items-center max-lg:justify-center">
+          <div className="flex w-fit items-center gap-2 rounded-full border-2 bg-white px-3 py-2 text-sm font-semibold text-black shadow-md">
+            <Crown size={20} />
+            Most Popular Product in DealDome
+          </div>
+          <div className="mt-3 flex w-full flex-col max-lg:items-center max-lg:justify-center">
+            <h1 className="max-w-[400px] text-balance text-2xl font-extrabold leading-none text-gray-900 md:text-3xl lg:max-w-[500px] lg:text-4xl">
+              Discover Perfect Tech,
+              <span className="text-main-violet-600">
+                {" "}
+                Delivered to Your Door
+              </span>
+            </h1>
+            <p className="mt-2 w-full max-w-[350px] text-balance text-sm text-gray-700 lg:max-w-[400px] lg:text-base">
+              Explore cutting-edge gadgets, premium devices, and innovative
+              solutions. Everything you need to stay ahead in the tech world,
+              just a click away.
+            </p>
+          </div>
+          <div className="mt-6 flex items-center gap-4">
+            <Button className="rounded-full py-5 font-bold">
+              <ShoppingCart strokeWidth={3} />
+              Shop Now
+            </Button>
+            <Button variant={"link"} className="text-slate-500">
+              Discover More
+            </Button>
+          </div>
         </div>
-
-        <div className="mt-3 flex w-full flex-col max-lg:items-center max-lg:justify-center">
-          <h1 className="max-w-[400px] text-balance text-2xl font-extrabold leading-none text-gray-900 md:text-3xl lg:max-w-[500px] lg:text-4xl">
-            Discover Perfect Tech,
-            <span className="text-main-violet-600">
-              {" "}
-              Delivered to Your Door
-            </span>
-          </h1>
-          <p className="mt-2 w-full max-w-[350px] text-balance text-sm text-gray-700 lg:max-w-[400px] lg:text-base">
-            Explore cutting-edge gadgets, premium devices, and innovative
-            solutions. Everything you need to stay ahead in the tech world, just
-            a click away.
-          </p>
-        </div>
-
-        <div className="mt-6 flex items-center gap-4">
-          <Button className="rounded-full py-5 font-bold">
-            <ShoppingCart strokeWidth={3} />
-            Shop Now
-          </Button>
-          <Button variant={"link"} className="text-slate-500">
-            Discover More
-          </Button>
+        {/* Hero Image */}
+        <div className="flex w-[80%] items-center justify-center lg:w-full">
+          <Image
+            src={"/assets/landing-macbook.png"}
+            width={1000}
+            height={600}
+            alt="Hero Image"
+            className="w-full"
+          />
         </div>
       </div>
 
-      {/* Hero Image */}
-      <div className="flex w-[80%] items-center justify-center lg:w-full">
-        <Image
-          src={"/assets/landing-macbook.png"}
-          width={1000}
-          height={600}
-          alt="Hero Image"
-          className="w-full"
-        />
+      <div className="mt-12 flex items-center justify-center">
+        <div className="grid grid-cols-2 gap-6 lg:grid-cols-4">
+          {LANDING_TESTIMONIALS.map((testi) => (
+            <div key={testi.name} className="flex items-center gap-4">
+              <div className="size-10 lg:size-12">
+                <Image
+                  src={testi.image}
+                  width={48}
+                  height={48}
+                  alt={testi.name}
+                />
+              </div>
+              <div>
+                <p className="line-clamp-1 text-sm font-bold md:text-base">
+                  {testi.comment}
+                </p>
+                <p className="line-clamp-1 text-sm md:text-base">
+                  {testi.name}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
--- constants.ts
+++ constants.ts
@@ -7,3 +7,25 @@ export const LANDING_PAGE_LINKS = [
   { url: "/testimonials", title: "Testimonials" },
   { url: "/rewards", title: "Rewards" },
 ];
+export const LANDING_TESTIMONIALS = [
+  {
+    name: "Matthew Marley",
+    comment: "Money saver!",
+    image: "/assets/landing-customer-1.png",
+  },
+  {
+    name: "Anthony Newman",
+    comment: "Best quality product!",
+    image: "/assets/landing-customer-2.png",
+  },
+  {
+    name: "Emily Lee",
+    comment: "Best deals",
+    image: "/assets/landing-customer-3.png",
+  },
+  {
+    name: "Arnash Priven",
+    comment: "Fast delivery",
+    image: "/assets/landing-customer-4.png",
+  },
+];
