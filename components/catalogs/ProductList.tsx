6df96848067139e99f41e06161c70e6cafabdc39
Dinann <notxurd46@gmail.com>
2025-02-25 13:32:36 +0700

    refactor(catalog): add image empty state to catalog when filtering products

--- components/catalogs/ProductList.tsx
+++ components/catalogs/ProductList.tsx
@@ -5,6 +5,7 @@ import { Filter, useFilterCatalog } from "@/hooks/use-filter-catalog";
 import { ProductDetailProps } from "@/types/product";
 import { useQuery } from "@tanstack/react-query";
 import ProductCard from "../card/ProductCard";
+import Image from "next/image";
 
 const fetchProducts = async (body?: Filter): Promise<ProductDetailProps[]> => {
   try {
@@ -30,6 +31,7 @@ function ProductList() {
 
   if (isLoading)
     return <div className="mt-6 w-full text-center">Loading...</div>;
+
   return (
     <div className="grid w-full grid-cols-2 md:grid-cols-3">
       {products && products.length > 0 ? (
@@ -37,8 +39,23 @@ function ProductList() {
           <ProductCard key={product.id + product.name} product={product} />
         ))
       ) : (
-        <div className="col-span-3 mt-6 w-full text-center">
-          No products found
+        <div className="col-span-full flex h-full flex-col items-center gap-2 text-center">
+          <Image
+            src={"/assets/empty-products.svg"}
+            width={500}
+            height={300}
+            alt="Products Not Found"
+            className="aspect-video size-[180px] lg:size-[280px]"
+            priority
+          />
+          <div className="space-y-0.5">
+            <h4 className="text-sm font-semibold md:text-base">
+              No Products Found
+            </h4>
+            <p className="max-w-md text-xs md:text-sm">
+              Showing the list of Products available on DealDome.
+            </p>
+          </div>
         </div>
       )}
     </div>
