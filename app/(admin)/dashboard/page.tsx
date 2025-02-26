92bafa6db3679f63c0dc15958b7a91884757c740
Dinann <notxurd46@gmail.com>
2025-02-26 23:09:05 +0700

    feat(dashboard): add dummy admin stats card

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,19 +1,32 @@
-import { getUser } from "@/lib/auth";
-import { validateProtected } from "@/lib/check-session";
+import { Package } from "lucide-react";
 import { Metadata } from "next";
-import React from "react";
 
 export const metadata: Metadata = {
   title: "Dashboard",
 };
 
 async function DashboardPage() {
-  const session = await validateProtected();
   return (
-    <>
-      {/* Dashboard Page Content */}
-      <div>Dashboard</div>
-    </>
+    <section className="w-full">
+      <div className="grid grid-cols-4 gap-4">
+        {Array.from({ length: 4 }, (_, i) => (
+          <div
+            key={i}
+            className="flex items-center justify-between rounded-lg bg-gradient-to-tr from-violet-600 to-indigo-600 p-4 text-white"
+          >
+            <div className="space-y-2">
+              <p className="text-xs font-semibold uppercase">Total Products</p>
+              <h4 className="text-5xl font-bold">
+                10 <span className="text-xs font-medium">Products</span>
+              </h4>
+            </div>
+            <div>
+              <Package className="opacity-60 md:size-20" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
 
