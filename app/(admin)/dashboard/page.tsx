6890e39b2be21b0738742446582a717991118139
Dinann <notxurd46@gmail.com>
2025-02-27 23:48:06 +0700

    style(dashboard): make dashboard cards section responsive

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -35,9 +35,9 @@ async function DashboardPage() {
         ))}
       </div>
 
-      <div className="grid h-full w-full gap-4 lg:grid-cols-12 lg:grid-rows-2">
+      <div className="grid h-full w-full gap-4 md:grid-cols-4 lg:grid-cols-12 lg:grid-rows-2">
         {/* Top Selling Products Card */}
-        <div className="lg:col-span-8 lg:row-span-2">
+        <div className="md:col-span-4 lg:col-span-8 lg:row-span-2">
           <Card className="h-full border-2">
             <CardHeader>
               <div className="flex items-center justify-between">
@@ -92,7 +92,7 @@ async function DashboardPage() {
         </div>
 
         {/* Total Customers Card */}
-        <div className="lg:col-span-4 lg:col-start-9">
+        <div className="md:col-span-2 lg:col-span-4 lg:col-start-9">
           <Card className="h-full border-2">
             <CardHeader>
               <CardTitle>Total Customers</CardTitle>
@@ -105,7 +105,7 @@ async function DashboardPage() {
         </div>
 
         {/* Sales by Country Card */}
-        <div className="lg:col-span-4 lg:col-start-9">
+        <div className="md:col-span-2 lg:col-span-4 lg:col-start-9">
           <Card className="h-full border-2">
             <CardHeader>
               <div className="flex items-center justify-between">
@@ -130,7 +130,12 @@ async function DashboardPage() {
                         {index + 1}.
                       </span>
                       <div>
-                        <p className="font-semibold">{country.name}</p>
+                        <p
+                          className="line-clamp-1 text-sm font-semibold lg:text-base"
+                          title={country.name}
+                        >
+                          {country.name}
+                        </p>
                         <div className="flex items-center gap-1">
                           <p className="text-xl font-bold text-main-violet-500">
                             {country.totalSales}
