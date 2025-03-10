087fa5d1ecec511138380f4e6d40cba34c08ec9d
Dinann <notxurd46@gmail.com>
2025-03-10 20:57:59 +0700

    style(login): remove other login option button in customer and admin login

--- components/auth/CustomerLoginForm.tsx
+++ components/auth/CustomerLoginForm.tsx
@@ -25,6 +25,7 @@ import { loginSchema } from "@/types/validations";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Checkbox } from "../ui/checkbox";
 import { SignIn } from "@/lib/actions/auth/customer/actions";
+import { Badge } from "../ui/badge";
 
 const initialState: LoginActionResult = {
   errors: {
@@ -94,8 +95,8 @@ export function CustomerLoginForm({
                     Tech Dome
                   </Link>
                 </div>
-                <div className="space-y-2">
-                  <h1 className="text-base font-bold md:text-2xl">
+                <div className="mt-4 space-y-2">
+                  <h1 className="text-xl font-bold md:text-2xl">
                     Welcome Back! 👋
                   </h1>
                   <p className="max-w-[300px] text-sm text-muted-foreground">
@@ -159,22 +160,6 @@ export function CustomerLoginForm({
                   </div>
                 </div>
 
-                <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-                  <span className="relative z-10 bg-background px-2 text-muted-foreground">
-                    Or continue with
-                  </span>
-                </div>
-                <div className="grid">
-                  <Button variant="outline" className="w-full">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                      <path
-                        d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                    <span className="sr-only">Login with Google</span>
-                  </Button>
-                </div>
                 <div className="text-center text-sm">
                   Don&apos;t have an account?{" "}
                   <Link
@@ -185,7 +170,7 @@ export function CustomerLoginForm({
                   </Link>
                 </div>
               </div>
-              <div className="mt-8 flex justify-end">
+              <div className="mt-8 flex justify-center">
                 <Link
                   href={"/admin/sign-in"}
                   className="flex items-center gap-2 text-sm font-semibold text-main-violet-500 duration-200 hover:text-main-violet-700 hover:underline"
